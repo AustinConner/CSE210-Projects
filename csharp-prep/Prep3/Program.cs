37d4a0b74745cd0a21177527e372d99f9dac6eb6
AustinConner <a.l.conner.2002@gmail.com>
2026-01-15 23:16:43 -0700

    prep 3 -guessing game.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Diagnostics;
 
-class Program
+string continueGame = "yes";
+
+while (continueGame == "yes")
 {
-    static void Main(string[] args)
+    Console.Write("Choosing magic number... ");
+    Random randomGenerator = new Random();
+    int magicNumber = randomGenerator.Next(1,11);
+    int guessCount = 0;
+    string userInput;
+    bool guessing = true;
+    while (guessing == true)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is your guess? ");
+        userInput = Console.ReadLine();
+        int userGuess = int.Parse(userInput);
+
+        guessCount += 1;
+
+        if (userGuess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (userGuess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (userGuess == magicNumber)
+        {
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {guessCount} tries!");
+            guessing = false;
+        }
     }
+    Console.WriteLine("Want to play again? Type 'yes'!");
+    userInput = Console.ReadLine();
+
+    if (userInput != "yes")
+    {
+        continueGame = "No";
+    }
+
+
 }
