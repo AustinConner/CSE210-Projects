fd948be4ed5c0b71ed58c5aea59729c34ad68fe8
AustinConner <a.l.conner.2002@gmail.com>
2026-01-22 22:05:13 -0700

    OOP Resume Example

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,3 +7,23 @@ class Program
         Console.WriteLine("Hello Develop02 World!");
     }
 }
+
+/* 
+Classes that I can see being useful:
+JournalEntry
+_Date:int
+_Content:str
+
+-------------------------------
+display()
+getResponse()
+
+Prompt
+_Question
+---------------------
+getQuestion
+
+SaveLoadEntries
+- +
+
+*/
