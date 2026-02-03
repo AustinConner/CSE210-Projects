d1549a85284b83e4db683687b6dfa5d3989ee500
AustinConner <a.l.conner.2002@gmail.com>
2026-02-03 09:58:20 -0700

    cash register in class demo

---
+++ sandbox/cash_register/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace cash_register;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ sandbox/cash_register/bin.cs
@@ -0,0 +1,27 @@
+// create bins to hold cash
+class Bin
+{
+    // attributes
+    private double _value;
+    private int _count;
+    private string _denomination;
+
+    // methods
+    // constructor
+    public Bin(double value, int count, string denomination)
+    {
+        _value = value;
+        _count = count;
+        _denomination = denomination;
+    }
+
+    public double TotalCash()
+    {
+        
+    } 
+
+    public void Exchange(int amount)
+    {
+        
+    }
+}
