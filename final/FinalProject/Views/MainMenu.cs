4b8f95807987d7d4db0f29b30c8d0ba72768bf1d
AustinConner <a.l.conner.2002@gmail.com>
2026-04-05 15:47:36 -0600

    Cleaned up folder structure some more.

--- final/FinalProject/Views/MainMenu.cs
+++ final/FinalProject/Views/MainMenu.cs
@@ -11,6 +11,7 @@ class MainMenu : View
             Console.WriteLine("1. View People");
             Console.WriteLine("2. Add Person");
             Console.WriteLine("3. Remove Person");
+<<<<<<< Updated upstream
             Console.WriteLine("4. Quit");
 
             int choice = InputService.GetMenuChoice(1, 4);
@@ -29,6 +30,30 @@ class MainMenu : View
                 case 4:
                 return ViewName.Quit;
 
+=======
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Load");
+            Console.WriteLine("6. Quit");
+
+            int choice = InputService.GetMenuChoice(1, 6);
+
+            switch (choice)
+            {
+                case 1: return ViewName.ViewPeople;
+                case 2: return ViewName.CreateNewPerson;
+                case 3: return ViewName.RemovePerson;
+                case 4:
+                    SaveLoadService.Save();
+                    Console.WriteLine("Press enter to continue...");
+                    Console.ReadLine();
+                    break;
+                case 5:
+                    SaveLoadService.Load();
+                    Console.WriteLine("Press enter to continue...");
+                    Console.ReadLine();
+                    break;
+                case 6: return ViewName.Quit;
+>>>>>>> Stashed changes
             } 
         }
     }
